826f044b356e37d4322125c0306a24af4589fcff
Abdugani0815 <abduvohidova863@gmail.com>
2024-09-12 12:41:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let ism = prompt("ismingizni kiriting...");
+let wrapper = document.querySelector(".wrapper");
+const fullName = document.createElement("h1");
+fullName.textContent = ism;
+wrapper.appendChild(ism);
+document.body.appendChild(div)
